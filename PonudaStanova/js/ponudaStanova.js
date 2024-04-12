ea065c4750a7fc0f523603a271f68f489e1264e0
DimitrijeBajac <dimitrijebajac77@gmail.com>
2024-04-12 14:26:46 +0200

    fixing stuff

--- PonudaStanova/js/ponudaStanova.js
+++ PonudaStanova/js/ponudaStanova.js
@@ -1,9 +1,10 @@
-function showDropdown(event) {
+function showDropdownFilter(event) {
     const dropdown = event.target.nextElementSibling; // Get the next sibling element, which is the dropdown content
     const span = event.currentTarget.querySelector('.dropdown-toggle');
     if (dropdown && dropdown.classList.contains('dropdown-content')) {
-        dropdown.classList.toggle('show');
+        dropdown.classList.toggle('show_filter');
         span.classList.toggle('highlight');
         console.log("done");
     }
 }
+
