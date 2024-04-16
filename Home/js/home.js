d9672d906dfdf1048c3b7218c830bfdd3b49260b
DimitrijeBajac <dimitrijebajac77@gmail.com>
2024-04-16 15:00:29 +0200

    responsive home,rapid essence, ponuda stanova

--- Home/js/home.js
+++ Home/js/home.js
@@ -5,3 +5,22 @@ function showDropdown(event) {
         console.log("done");
     }
 }
+
+function showDropdownSidebar(event) {
+    const dropdown = event.target.nextElementSibling; // Get the next sibling element, which is the dropdown content
+    if (dropdown && dropdown.classList.contains('dropdown-content')) {
+        dropdown.classList.toggle('show_sidebar');
+        console.log("done");
+    }
+}
+
+
+function showSidebar() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none'
+}
