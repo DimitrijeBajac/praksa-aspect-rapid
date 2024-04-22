fa24239729b927e2431dd8f91707eb6ffadd02ec
DimitrijeBajac <dimitrijebajac77@gmail.com>
2024-04-22 12:31:37 +0200

    navbar fix, home responsive tablet

--- Home/js/home.js
+++ Home/js/home.js
@@ -1,19 +1,44 @@
+document.addEventListener('click', function(event) {
+    const clickedElement = event.target;
+    if (!clickedElement.closest('.dropdown') && !clickedElement.matches('.dropdown-content')) {
+        closeAllDropdowns();
+    }
+});
+
+function closeAllDropdowns() {
+    const dropdowns = document.querySelectorAll('.dropdown-content');
+    dropdowns.forEach(function(dropdown) {
+        dropdown.classList.remove('show');
+        dropdown.classList.remove('show_sidebar');
+    });
+}
+
+
 function showDropdown(event) {
-    const dropdown = event.target.nextElementSibling; // Get the next sibling element, which is the dropdown content
+    const dropdown = event.target.nextElementSibling;
     if (dropdown && dropdown.classList.contains('dropdown-content')) {
-        dropdown.classList.toggle('show');
+        if (dropdown.classList.contains('show')) {
+            dropdown.classList.remove('show');
+        } else {
+            closeAllDropdowns(); 
+            dropdown.classList.add('show');
+        }
         console.log("done");
     }
 }
 
 function showDropdownSidebar(event) {
-    const dropdown = event.target.nextElementSibling; // Get the next sibling element, which is the dropdown content
+    const dropdown = event.target.nextElementSibling; 
     if (dropdown && dropdown.classList.contains('dropdown-content')) {
-        dropdown.classList.toggle('show_sidebar');
+        if (dropdown.classList.contains('show_sidebar')) {
+            dropdown.classList.remove('show_sidebar');
+        } else {
+            closeAllDropdowns();
+            dropdown.classList.add('show_sidebar');
+        }
         console.log("done");
     }
 }
-
 
 function showSidebar() {
     var sidebar = document.querySelector('.sidebar');
